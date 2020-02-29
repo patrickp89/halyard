47f3832a93fc149a249d2b4d2b7bdbf3960b3a11
patrickp89 <patrickpreuss@netherspace.de>
2020-02-29 18:50:12 +0100

    wip: barebone GTK window and input fields

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 An offline, stand-alone regular expression tester.
 
 ## ...but why?
-If you want to test regular expressions on any sensitive data you might not trust one of those ("popular"?) online RegEx testing tools. Halyard instead runs totally offline, is open-source and meant to be used stand-alone (i.e. its not a plugin for a browser that you do not use.) Its only dependency are the GTK+ libraries.
+If you want to test regular expressions on any sensitive data you might not trust one of those ("popular"?) online RegEx testing tools. Halyard instead runs totally offline, is open-source, and meant to be used stand-alone (i.e. its not a plugin for a browser that you do not use). Its only dependencies are the GTK+ libraries.
 
 ## How to build it?
 ### Dependencies
@@ -15,7 +15,7 @@ $ apt-get install libgtk-3-dev libgirepository1.0-dev
 Install [Cabal](https://www.haskell.org/cabal/) and build it:
 ```bash
 $ curl https://gitlab.haskell.org/haskell/ghcup/raw/master/bootstrap-haskell -sSf | sh
-$ cabal install haskell-gi gi-gtk
+$ cabal install happy haskell-gi gi-gtk
 $ cabal build
 ```
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,33 @@
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified GI.Gtk as Gtk
 import Data.GI.Base
 
 main :: IO ()
-main = putStrLn "Hello, halyard!"
+main =
+  do
+    Gtk.init Nothing
+    win <- new Gtk.Window [#title := "halyard - RegEx Tester"]
+    on win #destroy Gtk.mainQuit
+    #resize win 640 480
+    box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+    #add win box
+
+    -- the entry field for the regular expressions (with a default RegEx):
+    regExEntry <- new Gtk.Entry [#placeholderText := "a ([\\S]+) expression$"]
+    #packStart box regExEntry False False 0
+
+    -- create a TextView, get the its buffer and set a default text:
+    testTextEntry <- new Gtk.TextView [#wrapMode := Gtk.WrapModeWordChar]
+    buf <- testTextEntry `get` #buffer
+    --  TODO: buf `set_text` "Test eins zwei drei"
+    #packStart box testTextEntry False False 0
+
+    -- a button (TODO: remove later and use an event on the entry level):
+    btn <- new Gtk.Button [#label := "Test expression"]
+    #packStart box btn False False 0
+    
+    #showAll win
+    Gtk.main
