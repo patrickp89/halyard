707a04858b8b4af2485a89fccac5fb962c07a4fc
patrickp89 <patrickpreuss@netherspace.de>
2020-05-31 13:41:56 +0200

    fix: use base package version 4.13 for GHC 8.8.x

--- src/Main.hs
+++ src/Main.hs
@@ -9,9 +9,13 @@ main :: IO ()
 main =
   do
     Gtk.init Nothing
+
+    -- create the window:
     win <- new Gtk.Window [#title := "halyard - RegEx Tester"]
     on win #destroy Gtk.mainQuit
     #resize win 640 480
+
+    -- and a vbox:
     box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
     #add win box
 
@@ -19,15 +23,22 @@ main =
     regExEntry <- new Gtk.Entry [#placeholderText := "a ([\\S]+) expression$"]
     #packStart box regExEntry False False 0
 
-    -- create a TextView, get the its buffer and set a default text:
-    testTextEntry <- new Gtk.TextView [#wrapMode := Gtk.WrapModeWordChar]
-    buf <- testTextEntry `get` #buffer
-    --  TODO: buf `set_text` "Test eins zwei drei"
-    #packStart box testTextEntry False False 0
+
+    -- next, we need a multi-line input field that will collect text to
+    -- match the provided RegEx against; first, a text buffer:
+    buf <- new Gtk.TextBuffer []
+    #setText buf "your test text goes here :)" (-1)
+
+    -- ...then the actual text view:
+    testStringsTextEntry <- Gtk.textViewNewWithBuffer buf
+
+    -- TODO: scroll bars etc.!
+    #packStart box testStringsTextEntry False False 0
 
     -- a button (TODO: remove later and use an event on the entry level):
-    btn <- new Gtk.Button [#label := "Test expression"]
+    btn <- new Gtk.Button [#label := "Test expression!"]
     #packStart box btn False False 0
+    -- on btn #clicked (set msg [#label := "Clickeddd"])
     
     #showAll win
     Gtk.main
